fbf1c9686eaf2e59d50f017288050a068123781a
Manikanta111223 <86046616+Manikanta111223@users.noreply.github.com>
2021-06-17 13:58:10 +0530

    removed file location config

--- src/main/java/com/gapbot/demo/DemoApplication.java
+++ src/main/java/com/gapbot/demo/DemoApplication.java
@@ -9,16 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class DemoApplication implements WebMvcConfigurer {
+public class DemoApplication {
 
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-		// Register resource handler for images
-		registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
-				.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
